1058977f4195973ffeef3d96e115c53eeb45385f
Amalina <azamalina@maxis.com.my>
2024-08-11 09:14:33 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'todo-app',
+  webDir: 'www'
+};
+
+export default config;
